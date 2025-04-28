11746af9e9f8abc4eee832977764eeda490c5dbb
Abraham-Zambrano <lu.zab.vl@gmail.com>
2025-04-28 05:19:11 -0500

    Update auth.service.ts

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,4 +1,3 @@
-// auth.service.ts
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -7,20 +6,33 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private users = [
+    { username: 'AZAMBRANO', password: 'admin123', name: 'Abraham Zambrano' },
+    { username: 'JLINDAO', password: 'user123', name: 'Joseph Lindao' }
+  ];
 
-  private currentUserSubject = new BehaviorSubject<any>(null);
-  currentUser$ = this.currentUserSubject.asObservable();
+  private currentUser = new BehaviorSubject<any>(null);
+  currentUser$ = this.currentUser.asObservable();
 
   constructor(private router: Router) {}
 
+  login(username: string, password: string): boolean {
+    const user = this.users.find(u => u.username === username && u.password === password);
+    if (user) {
+      this.currentUser.next(user);
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      return true;
+    }
+    return false;
+  }
+
   logout(): void {
-    this.currentUserSubject.next(null);
+    this.currentUser.next(null);
     localStorage.removeItem('currentUser');
     this.router.navigate(['/login']);
   }
 
   getCurrentUser(): any {
-    return this.currentUserSubject.value;
+    return this.currentUser.value || JSON.parse(localStorage.getItem('currentUser')!);
   }
 }
