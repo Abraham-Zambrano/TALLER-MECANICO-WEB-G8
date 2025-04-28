d3c7bd624b1a86132c52d4a7afaaafb38c1647e2
Abraham-Zambrano <lu.zab.vl@gmail.com>
2025-04-28 05:21:21 -0500

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../.././auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,  
+  imports: [MatToolbarModule, MatButtonModule, CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(public authService: AuthService) {}
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
