4abddc8fb537e3920fd85b5df15a9cf370db5292
Abraham-Zambrano <lu.zab.vl@gmail.com>
2025-04-28 05:23:01 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home-principal/home-principal.component';
+import { authGuard } from './../auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' }
+];
